b4ba6a203d049664beba4da121d1d57d17a01958
santi <sdvillal@gmail.com>
2017-10-04 00:38:24 +0200

    link statically to libjpeg-turbo

--- recipe/build.sh
+++ recipe/build.sh
@@ -67,7 +67,10 @@ cmake -LAH                                                                \
     -DBUILD_PNG=0                                                         \
     -DBUILD_OPENEXR=1                                                     \
     -DBUILD_JASPER=0                                                      \
+    -DWITH_JPEG=1                                                         \
     -DBUILD_JPEG=0                                                        \
+    -DJPEG_INCLUDE_DIR=$PREFIX/include                                    \
+    -DJPEG_LIBRARY=$PREFIX/lib/libjpeg.a                                  \
     -DWITH_CUDA=0                                                         \
     -DWITH_OPENCL=0                                                       \
     -DWITH_OPENNI=0                                                       \
