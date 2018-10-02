5bc5b2d0a51aed062c90c538d03de7f13c92553d
santi <sdvillal@gmail.com>
2018-10-02 11:43:08 +0200

    improve local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-CONFIG="linux_python2.7" .circleci/run_docker_build.sh
-CONFIG="linux_python3.5" .circleci/run_docker_build.sh
-CONFIG="linux_python3.6" .circleci/run_docker_build.sh
+
+# Can run from anywhere...
+pushd `dirname $0` > /dev/null
+myDir=`pwd`
+popd > /dev/null
+
+# Build package variants
+CONFIG="linux_python2.7" ${myDir}/.circleci/run_docker_build.sh
+CONFIG="linux_python3.6" ${myDir}/.circleci/run_docker_build.sh
+
+# Clearn cruft
+rm -f ${myDir}/.ci_support/clobber_*
